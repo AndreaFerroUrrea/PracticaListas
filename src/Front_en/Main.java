39098d8dbdabdff0884815b0423281b8c488e36d
AlliSerna23231 <allison_serna23231@elpoli.edu.co>
2024-03-09 11:49:31 -0500

    Paquetes principales

---
+++ src/Front_en/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Front_en;
+
+/**
+ *
+ * @author allil
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        FramePolF3 f1 = new FramePolF3();
+        f1.setVisible(true);
+    }
+}
